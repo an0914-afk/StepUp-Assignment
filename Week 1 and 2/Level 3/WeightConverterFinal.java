63be4c1208490831523aeaf9bffc800fede05108
aadityanarayann <aadityanarayan11016@gmail.com>
2026-02-26 16:58:19 +0530

    Level 3 week 1 and 2 completed

---
+++ Week 1 and 2/Level 3/WeightConverterFinal.java
@@ -0,0 +1,32 @@
+// Program to Convert Weight from Pounds to Kilograms
+// Author: Aaditya
+// Date: 23.02.26
+
+import java.util.Scanner;
+
+public class WeightConverterFinal {
+    public static void main(String[] args) {
+
+        // Create Scanner Object for User Input
+        Scanner input = new Scanner(System.in);
+
+        // Conversion Factor - 1 pound = 2.2 kg
+        double poundsToKgFactor = 2.2;
+
+        // Take User Input for Weight in Pounds
+        System.out.print("Enter the weight of the person in pounds : ");
+        double weightInPounds = input.nextDouble();
+
+        // Calculate Weight in Kilograms
+        // Formula: weightInKg = weightInPounds / poundsToKgFactor
+        double weightInKg = weightInPounds / poundsToKgFactor;
+
+        // Output - Matches Exact Output Format from Question
+        // O/P => The weight of the person in pound is ___ and in kg is ___
+        System.out.println("The weight of the person in pound is " + weightInPounds +
+                           " and in kg is " + weightInKg);
+
+        // Close Scanner
+        input.close();
+    }
+}
